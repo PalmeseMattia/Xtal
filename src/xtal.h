3e4280921594154093f9565446a3405de6074ab0
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-06-21 02:12:26 +0200

    fix indentation

--- src/xtal.h
+++ src/xtal.h
@@ -21,13 +21,13 @@ void run_tests();
 /*
  * ASSERTIONS
  */
-#define ASSERT_EQUAL_INT(expected, actual) \
+# define ASSERT_EQUAL_INT(expected, actual) \
 	if (expected != actual) { \
 		fprintf(stderr, "Assertion failed: expected %d, got %d\n", expected, actual); \
 		exit(EXIT_FAILURE); \
 	}
 
-#define ASSERT_EQUAL_STR(expected, actual) \
+# define ASSERT_EQUAL_STR(expected, actual) \
 	for (int i = 0; *(expected + i) ;i++) { \
 		if (*(expected + i) != *(actual + i) || !(actual + i)) { \
 			fprintf(stderr, "Assertion failed: Strings differ at index %d.\nExpected \"%s\", got \"%s\"\n", i, expected, actual); \
@@ -35,7 +35,7 @@ void run_tests();
 		} \
 	} \
 
-#define ASSERT_TRUE(condition) \
+# define ASSERT_TRUE(condition) \
 	if (!condition) { \
 		fprintf(stderr, "Assertion failed: Condition is false\n"); \
 		exit(EXIT_FAILURE); \
@@ -50,8 +50,8 @@ void run_tests();
 # define CHECK "\u2713"
 # define CROSS "\u2715"
 # define PRINT_PASSED printf("%sTest passed %s%s\n\n", GREEN, CHECK, NRM)
-#define PRINT_FAILED_SIG printf("%sTest failed with signal %d and status %d %s%s\n\n", RED, WTERMSIG(status), WEXITSTATUS(status), CROSS, NRM)
+# define PRINT_FAILED_SIG printf("%sTest failed with signal %d and status %d %s%s\n\n", RED, WTERMSIG(status), WEXITSTATUS(status), CROSS, NRM)
 # define PRINT_SEG_FAULT printf("%sTest failed due to segmentation fault %s%s\n\n", RED, CROSS, NRM)
-#define PRINT_FAILED printf("%sTest failed with status %d %s%s\n\n", RED, WEXITSTATUS(status), CROSS, NRM)
+# define PRINT_FAILED printf("%sTest failed with status %d %s%s\n\n", RED, WEXITSTATUS(status), CROSS, NRM)
 
 #endif
