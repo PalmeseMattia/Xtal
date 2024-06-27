b79dd12be8826f74f9cb721b4599937a3361eb53
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-06-27 15:18:30 +0200

    made it single header

--- example/example2.c
+++ example/example2.c
@@ -5,13 +5,18 @@ TEST(test_one) {
 }
 
 TEST(test_two) {
-	ASSERT_EQUAL_INT(10, 15);
+	ASSERT_EQUAL_INT(10, 10);
 }
 
 TEST(test_three) {
 	int *p = NULL;
 	*p = 42;
 	ASSERT_EQUAL_INT(42, *p);
+}
+
+//THIS TEST WILL FAIL
+TEST(test_four) {
+	ASSERT_EQUAL_STR("Hello", "Hell");
 }
 
 int main() {
--- src/xtal.h
+++ src/xtal.h
@@ -13,10 +13,67 @@
     void name(); \
     __attribute__((constructor)) void register_##name() { register_test(name); } \
     void name()
+/*
+ * PRINT UTILITIES 
+ */
+# define GREEN "\x1B[32m"
+# define RED "\x1B[31m"
+# define NRM "\x1B[0m"
+# define CHECK "\u2713"
+# define CROSS "\u2715"
+# define PRINT_PASSED printf("%sTest passed %s%s\n\n", GREEN, CHECK, NRM)
+# define PRINT_FAILED_SIG printf("%sTest failed with signal %d and status %d %s%s\n\n", RED, WTERMSIG(status), WEXITSTATUS(status), CROSS, NRM)
+# define PRINT_SEG_FAULT printf("%sTest failed due to segmentation fault %s%s\n\n", RED, CROSS, NRM)
+# define PRINT_FAILED printf("%sTest failed with status %d %s%s\n\n", RED, WEXITSTATUS(status), CROSS, NRM)
 
 typedef void (*testcase)();
-void register_test(testcase test);
-void run_tests();
+
+static int	test_count = 0;
+static int	register_size = 0;
+static testcase	*tests = NULL;
+
+
+void register_test(testcase test)
+{
+	if (test_count >= (register_size - 1)) {
+		tests = realloc(tests, sizeof(testcase) * (register_size += BUFFER_SIZE));
+	}
+	*(tests + test_count) = test;
+	test_count++;
+}
+
+void run_tests()
+{
+	int	pid;
+	int	status;
+
+	for (int i = 0; i < test_count; i++) {
+		if ((pid = fork()) < 0) {
+			perror("Fork failed");
+			exit(1);
+		}
+		if (pid == 0) {
+			(*(tests + i))();
+			exit(0);
+		}
+		else {
+			printf("Running test %d\n", i + 1);
+			waitpid(pid, &status, 0);
+			if (WIFSIGNALED(status)) {
+				if (WTERMSIG(status) == SIGSEGV)
+					PRINT_SEG_FAULT;
+				else
+					PRINT_FAILED_SIG;
+			}
+			else if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+				PRINT_PASSED;
+			else
+				PRINT_FAILED;
+		}
+	}
+	free(tests);
+	tests = NULL;
+}
 
 /*
  * ASSERTIONS
@@ -40,18 +97,5 @@ void run_tests();
 		fprintf(stderr, "Assertion failed: Condition is false\n"); \
 		exit(EXIT_FAILURE); \
 	}
-
-/*
- * PRINT UTILITIES 
- */
-# define GREEN "\x1B[32m"
-# define RED "\x1B[31m"
-# define NRM "\x1B[0m"
-# define CHECK "\u2713"
-# define CROSS "\u2715"
-# define PRINT_PASSED printf("%sTest passed %s%s\n\n", GREEN, CHECK, NRM)
-# define PRINT_FAILED_SIG printf("%sTest failed with signal %d and status %d %s%s\n\n", RED, WTERMSIG(status), WEXITSTATUS(status), CROSS, NRM)
-# define PRINT_SEG_FAULT printf("%sTest failed due to segmentation fault %s%s\n\n", RED, CROSS, NRM)
-# define PRINT_FAILED printf("%sTest failed with status %d %s%s\n\n", RED, WEXITSTATUS(status), CROSS, NRM)
 
 #endif
